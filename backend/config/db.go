f5472e1f2af3da3508c5f309d1c4ff2731998ddd
Michal Gugala <michal.gugala@icloud.com>
2023-11-11 00:57:09 +0100

    remove custom pass, add gorm, add model, create a POST /api/todo

--- backend/config/db.go
+++ backend/config/db.go
@@ -7,8 +7,8 @@ import (
 	"gorm.io/gorm"
 )
 
-func getDatabaseConnection() *gorm.DB {
-	dsn := "host=localhost user=postgress password=pass dbname=todo port=5433 sslmode=disable TimeZone=Europe/Warsaw"
+func GetDatabaseConnection() *gorm.DB {
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5433 sslmode=disable TimeZone=Europe/Warsaw"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(fmt.Sprintf("Cannot connect to DB: %s", err))
--- backend/todo/routes.go
+++ backend/todo/routes.go
@@ -4,4 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(r *gin.Engine) {
 	r.GET("/api/helloTodo", GetTodoHelloHandler)
+	r.POST("/api/todo", PostTodo)
 }
