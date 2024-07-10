401d7f497e73b9a6baaa8c8b08f81f6068c89ba8
Michał Gugała <noshar951@gmail.com>
2024-07-10 13:50:29 +0200

    fix script

--- backend/config/db.go
+++ backend/config/db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetDatabaseConnection() *gorm.DB {
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5433 sslmode=disable TimeZone=Europe/Warsaw"
+	dsn := "host=db user=postgres password=postgres dbname=postgres port=5433 sslmode=disable TimeZone=Europe/Warsaw"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(fmt.Sprintf("Cannot connect to DB: %s", err))
