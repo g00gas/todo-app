fb94a26738e1e0bc830fca403e2f021855ebae42
Michał Gugała <noshar951@gmail.com>
2024-07-10 15:04:48 +0200

    fix ports

--- todo_frontend/src/api/apiClient.ts
+++ todo_frontend/src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: `http://${import.meta.env.BASE_URL}:3000/api`
+    baseURL: `http://${import.meta.env.BASE_URL}:8080/api`
 })
 
 export default apiClient
